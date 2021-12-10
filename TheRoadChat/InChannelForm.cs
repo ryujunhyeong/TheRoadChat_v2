d1012f22e22ca7a79916d53885818d8268f2ac4c
zktm9903 <zktm9903@naver.com>
2021-12-10 15:20:59 +0900

    bug fix

--- TheRoadChat/InChannelForm.cs
+++ TheRoadChat/InChannelForm.cs
@@ -60,7 +60,7 @@ namespace TheRoadChat
                 {
                     if (this.i_user != messageinfo.i_user)
                     {
-                        opponentBubble Bubble = new opponentBubble(i_user, messageinfo.user_name, messageinfo.msg, messageinfo.m_dt);
+                        opponentBubble Bubble = new opponentBubble(messageinfo.i_user, messageinfo.user_name, messageinfo.msg, messageinfo.m_dt);
                         PanelMsg.Controls.Add(Bubble);
                         searchMsg1.Add(Bubble);
                     }
@@ -209,73 +209,79 @@ namespace TheRoadChat
             string fileExtension = string.Empty;
             string fileType = string.Empty;
 
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            try
             {
-                openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK) //경로 선택하는 창띄우기
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
-                    //Get the path of specified file
-                    filePath = openFileDialog.FileName; //파일 경로 저장
-                    fileName = openFileDialog.SafeFileName; //파일 이름 저장
-                    //Console.WriteLine(filePath);
-                    //Console.WriteLine(openFileDialog.SafeFileName);
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK) //경로 선택하는 창띄우기
+                    {
+                        //Get the path of specified file
+                        filePath = openFileDialog.FileName; //파일 경로 저장
+                        fileName = openFileDialog.SafeFileName; //파일 이름 저장
+                                                                //Console.WriteLine(filePath);
+                                                                //Console.WriteLine(openFileDialog.SafeFileName);
+                    }
+                }
+
+                fileExtension = System.IO.Path.GetExtension(filePath);
+
+                switch (fileExtension.ToLower())
+                {
+                    case ".bmp":
+                    case ".exif":
+                    case ".gif":
+                    case ".jpg":
+                    case ".jpeg":
+                    case ".png":
+                    case ".tif":
+                    case ".tiff":
+                        fileType = "img";
+                        break;
+                    default:
+                        fileType = "not img";
+                        break;
+                }
+
+                UInt32 FileSize; //이미지 파일 사이즈
+                byte[] rawData; //이미지 파일 바이트 배열
+                FileStream fs; //파일스트림
+
+
+                fs = new FileStream(@filePath, FileMode.Open, FileAccess.Read); //파일 열기
+                FileSize = (UInt32)fs.Length; //파일 크기 받기
+
+                rawData = new byte[FileSize]; //파일 크기만큼의 바이트 배열 생성
+                fs.Read(rawData, 0, (int)FileSize); //파일 읽기
+                fs.Close(); //파일 스트림 종료
+
+                //db에 메뉴 정보 삽입 후 결과 받기
+                int my_i_file = DBManager.thisDBManager.pushFile(this.i_user, this.i_channel, fileName, FileSize, rawData);
+
+                socketIO.client.Emit("chat message", new messageInfo(this.i_user, this.i_channel, chat.myName, fileType, DateTime.Now.ToString("HHmmss"), my_i_file));
+
+                messages.Add(new messageInfo(this.i_user, this.i_channel, chat.myName, fileType, DateTime.Now.ToString("HHmmss"), my_i_file));
+
+                if (fileType.CompareTo("img") == 0)
+                {
+                    myImgBubble Bubble = new myImgBubble(filePath, fileType, DateTime.Now.ToString("HHmmss"));
+                    PanelMsg.Controls.Add(Bubble);
+                    PanelMsg.ScrollControlIntoView(Bubble);
+                }
+                else
+                {
+                    myFileBubble Bubble = new myFileBubble(my_i_file, DateTime.Now.ToString("HHmmss"));
+                    PanelMsg.Controls.Add(Bubble);
+                    PanelMsg.ScrollControlIntoView(Bubble);
                 }
             }
+            catch { }
 
-            fileExtension = System.IO.Path.GetExtension(filePath);
-
-            switch (fileExtension.ToLower())
-            {
-                case ".bmp":
-                case ".exif":
-                case ".gif":
-                case ".jpg":
-                case ".jpeg":
-                case ".png":
-                case ".tif":
-                case ".tiff":
-                    fileType = "img";
-                    break;
-                default:
-                    fileType = "not img";
-                    break;
-            }
-
-            UInt32 FileSize; //이미지 파일 사이즈
-            byte[] rawData; //이미지 파일 바이트 배열
-            FileStream fs; //파일스트림
             
-
-            fs = new FileStream(@filePath, FileMode.Open, FileAccess.Read); //파일 열기
-            FileSize = (UInt32)fs.Length; //파일 크기 받기
-
-            rawData = new byte[FileSize]; //파일 크기만큼의 바이트 배열 생성
-            fs.Read(rawData, 0, (int)FileSize); //파일 읽기
-            fs.Close(); //파일 스트림 종료
-
-            //db에 메뉴 정보 삽입 후 결과 받기
-            int my_i_file = DBManager.thisDBManager.pushFile(this.i_user, this.i_channel, fileName, FileSize, rawData);
-
-            socketIO.client.Emit("chat message", new messageInfo(this.i_user, this.i_channel, chat.myName, fileType, DateTime.Now.ToString("HHmmss"), my_i_file));
-
-            messages.Add(new messageInfo(this.i_user, this.i_channel, chat.myName, fileType, DateTime.Now.ToString("HHmmss"), my_i_file));
-
-            if(fileType.CompareTo("img") == 0)
-            {
-                myImgBubble Bubble = new myImgBubble(filePath, fileType, DateTime.Now.ToString("HHmmss"));
-                PanelMsg.Controls.Add(Bubble);
-                PanelMsg.ScrollControlIntoView(Bubble);
-            }
-            else
-            {
-                myFileBubble Bubble = new myFileBubble(my_i_file, DateTime.Now.ToString("HHmmss"));
-                PanelMsg.Controls.Add(Bubble);
-                PanelMsg.ScrollControlIntoView(Bubble);
-            }
 
         }
 
